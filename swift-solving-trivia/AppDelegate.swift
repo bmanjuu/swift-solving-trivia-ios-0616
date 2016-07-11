c44cb2439067b6e61ee4b3e0e0605da70c2aa019
bmanjuu <bettyofung@gmail.com>
2016-07-11 18:00:33 -0400

    set up dictionary

--- swift-solving-trivia/AppDelegate.swift
+++ swift-solving-trivia/AppDelegate.swift
@@ -8,15 +8,78 @@
 
 import UIKit
 
-@UIApplicationMain
+UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
-    /*
-     
-     write your function here
-     
-     */
-
+    //What is the only state whose capital contains none of the characters as the state name?
+    
+    let statesAndCapitals = [
+        "Alabama" : "Montgomery",
+        "Alaska" : "Juneau",
+        "Arizona" : "Phoenix",
+        "Arkansas" : "Little Rock",
+        "California" : "Sacramento",
+        "Colorado" : "Denver",
+        "Connecticut" : "Hartford",
+        "Delaware" : "Dover",
+        "Florida" : "Tallahassee",
+        "Georgia" : "Atlanta",
+        "Hawaii" : "Honolulu",
+        "Idaho" : "Boise",
+        "Illinois" : "Springfield",
+        "Indiana" : "Indianapolis",
+        "Iowa" : "Des Moines",
+        "Kansas" : "Topeka",
+        "Kentucky" : "Frankfort",
+        "Louisiana" : "Baton Rouge",
+        "Maine" : "Augusta",
+        "Maryland" : "Annapolis",
+        "Massachusetts" : "Boston",
+        "Michigan" : "Lansing",
+        "Minnesota" : "Saint Paul",
+        "Mississippi" : "Jackson",
+        "Missouri" : "Jefferson City",
+        "Montana" : "Helena",
+        "Nebraska" : "Lincoln",
+        "Nevada" : "Carson City",
+        "New Hampshire" : "Concord",
+        "New Jersey" : "Trenton",
+        "New Mexico" : "Santa Fe",
+        "New York" : "Albany",
+        "North Carolina" : "Raleigh",
+        "North Dakota" : "Bismarck",
+        "Ohio" : "Columbus",
+        "Oklahoma" : "Oklahoma City",
+        "Oregon" : "Salem",
+        "Pennsylvania" : "Harrisburg",
+        "Rhode Island" : "Providence",
+        "South Carolina" : "Columbia",
+        "South Dakota" : "Pierre",
+        "Tennessee" : "Nashville",
+        "Texas" : "Austin",
+        "Utah" : "Salt Lake City",
+        "Vermont" : "Montpelier",
+        "Virginia" : "Richmond",
+        "Washington" : "Olympia",
+        "West Virginia" : "Charleston",
+        "Wisconsin" : "Madison",
+        "Wyoming" : "Cheyenne",
+        ]
+    
+    func solveTrivia(triviaDictionary: [String:String]) -> String {
+        
+        for (state, capital) in triviaDictionary {
+            let stateLetters = state.characters
+            let capitalLetters = capital.characters
+            
+            print(stateLetters)
+            print(capitalLetters)
+        }
+        
+        return "COMPLETE THE FUNCTION"
+    }
+    
+//THE END
 }
 
